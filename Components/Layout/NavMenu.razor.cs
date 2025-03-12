dcbaa7e9afb2d633b57e85f42017737299f143e6
sfr71 <sfr71@uclive.ac.nz>
2025-03-12 20:45:02 +1300

    Basic template for EEWR

---
+++ Components/Layout/NavMenu.razor.cs
@@ -0,0 +1,20 @@
+// using Microsoft.AspNetCore.Components.Authorization;
+// using Microsoft.AspNetCore.Components;
+// using Microsoft.AspNetCore.Http;
+// using System.Diagnostics;
+//
+// namespace EpicElectricWorldRecords.Components.Layout
+// {
+//     public partial class NavMenu
+//     {
+//         [Inject] private AuthenticationStateProvider AuthStateProvider { get; set; }
+//     
+//         private bool IsAuthenticated;
+//         private string? UserName;
+//
+//         protected override async Task OnInitializedAsync()
+//         {
+//             
+//         }
+//     }
+// }
